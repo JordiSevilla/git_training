3413fe6707e2467d5404aba0ec06c8a7a152d836
JordiSevilla <jordi.sevilla.7e4@itb.cat>
2020-11-05 09:35:34 +0100

    Prueba

---
+++ Prueba/If.java
@@ -0,0 +1,11 @@
+public class If{
+	public static void main(String[] args){
+		boolean a=true;
+
+		if(a == true){
+		System.out.println("Hola buenos dias");
+		} else
+			System.out.println("Adios");
+
+	}
+}
